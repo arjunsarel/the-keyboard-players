8d89e288add5964da4a8401ffe5185ee0b24804a
Ajun Sarel <arjunsarel@gmail.com>
2022-04-30 13:37:46 +0530

    Add files via upload
    Initial commit

---
+++ adidas-backend/src/main/java/the/keyboard/players/AdidasBackendApplication.java
@@ -0,0 +1,13 @@
+package the.keyboard.players;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdidasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdidasBackendApplication.class, args);
+	}
+
+}
---
+++ adidas-backend/src/test/java/the/keyboard/players/AdidasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package the.keyboard.players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdidasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
